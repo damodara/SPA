cfc3cc679094195c270ae37ee671a04e3eda57df
damodara <jay108@mail.ru>
2026-02-21 22:08:57 +0300

    crud generic

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("courses/",  include("courses.urls", namespace="courses")),
+    path("courses/", include("courses.urls", namespace="courses")),
 ]
