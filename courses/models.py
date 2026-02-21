5e007550c98de6d743c98b06ac608ffde73dc53e
damodara <jay108@mail.ru>
2026-02-21 15:44:59 +0300

    создал модель курсов и уроков и сделал миграции

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название курса")
+    description = models.TextField(verbose_name="Описание курса")
+    preview = models.ImageField(upload_to="courses/image", verbose_name="Превью")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название урока")
+    description = models.TextField(verbose_name="Описание урока")
+    preview = models.ImageField(upload_to="courses/image", verbose_name="Превью")
+    video_link = models.URLField(unique=True, verbose_name="Ссылка на видео")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
