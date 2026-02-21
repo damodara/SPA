316dee585adb88624019cc1cb6c64aac6cd949f2
damodara <jay108@mail.ru>
2026-02-21 21:50:41 +0300

    crud Viewsets

--- courses/models.py
+++ courses/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название курса")
-    description = models.TextField(verbose_name="Описание курса")
-    preview = models.ImageField(upload_to="courses/image", verbose_name="Превью")
+    description = models.TextField(verbose_name="Описание курса", blank=True, null=True)
+    preview = models.ImageField(upload_to="courses/image", verbose_name="Превью", blank=True, null=True)
 
     class Meta:
         verbose_name = "Курс"
@@ -13,9 +13,9 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название урока")
-    description = models.TextField(verbose_name="Описание урока")
-    preview = models.ImageField(upload_to="courses/image", verbose_name="Превью")
-    video_link = models.URLField(unique=True, verbose_name="Ссылка на видео")
+    description = models.TextField(verbose_name="Описание урока", blank=True, null=True)
+    preview = models.ImageField(upload_to="courses/image", verbose_name="Превью", blank=True, null=True)
+    video_link = models.URLField(unique=True, verbose_name="Ссылка на видео", blank=True, null=True)
 
     class Meta:
         verbose_name = "Урок"
